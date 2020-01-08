2c7fbfdc265ae2317b469f45687054e64029fced
Yangkai.Shen <237497819@qq.com>
2020-01-08 10:45:17 +0800

    :art: 格式化代码

--- src/main/java/com/xkcoding/magicstarteriddemo/config/IdConfig.java
+++ src/main/java/com/xkcoding/magicstarteriddemo/config/IdConfig.java
@@ -22,24 +22,51 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class IdConfig {
-    @Bean
-    public Id snowflakeId() {
-        return SnowflakeIdFactory.create().dataCenterId(1L).workerId(1L).prefix(new DefaultPrefix("2019-")).getInstance();
-    }
+@Bean
+public Id snowflakeId() {
+    return SnowflakeIdFactory.create()
+            .dataCenterId(1L)
+            .workerId(1L)
+            .prefix(new DefaultPrefix("2019-"))
+            .getInstance();
+}
 
-    @Bean
-    public Id databaseId1(DataSource dataSource) {
-        return DatabaseIdFactory.create().businessName(() -> String.format("test_db_%s", DateUtil.today())).prefix(() -> "2019-").dataSource(dataSource).step(1).stepStart(100000).retryTimes(3).tableName("id_test").getInstance();
-    }
+@Bean
+public Id databaseId1(DataSource dataSource) {
+    return DatabaseIdFactory.create()
+            .businessName(() -> String.format("test_db_%s", DateUtil.today()))
+            .prefix(() -> "2019-")
+            .dataSource(dataSource)
+            .step(1)
+            .stepStart(100000)
+            .retryTimes(3)
+            .tableName("id_test")
+            .getInstance();
+}
 
     @Bean
     public Id databaseId2(DataSource dataSource, IdDatabaseProperties databaseProperties) {
-        return DatabaseIdFactory.create().businessName(() -> String.format("test_db_2_%s", DateUtil.today())).prefix(() -> "2019-2-").dataSource(dataSource).step(databaseProperties.getStep()).stepStart(databaseProperties.getStepStart()).retryTimes(databaseProperties.getRetryTimes()).tableName(databaseProperties.getTableName()).getInstance();
+        return DatabaseIdFactory.create()
+                .businessName(() -> String.format("test_db_2_%s", DateUtil.today()))
+                .prefix(() -> "2019-2-")
+                .dataSource(dataSource)
+                .step(databaseProperties.getStep())
+                .stepStart(databaseProperties.getStepStart())
+                .retryTimes(databaseProperties.getRetryTimes())
+                .tableName(databaseProperties.getTableName())
+                .getInstance();
     }
 
-    @Bean
-    public Id redisId() {
-        return RedisIdFactory.create().businessName(() -> String.format("test_redis_%s", DateUtil.today())).prefix(() -> "2019-").ip("localhost").port(6379).step(1).stepStart(0).getInstance();
-    }
+@Bean
+public Id redisId() {
+    return RedisIdFactory.create()
+            .businessName(() -> String.format("test_redis_%s", DateUtil.today()))
+            .prefix(() -> "2019-")
+            .ip("localhost")
+            .port(6379)
+            .step(1)
+            .stepStart(0)
+            .getInstance();
+}
 
 }
